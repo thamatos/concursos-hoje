ff48569c4c322052eba83fd8457f3bb657c7eb56
thamatos <98627093+thamatos@users.noreply.github.com>
2023-03-13 21:36:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-  return "Olá, mundo! Esse é meu site. (Thaís Matos)
+  return "Olá, mundo! Esse é meu site. (Thaís Matos)"
